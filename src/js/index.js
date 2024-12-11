13ec189a0b0e550b23f74dcd58a9b0389dfd4062
Pablo Querales <pabloquerales20@gmail.com>
2024-12-11 23:10:56 +0100

    debuging de código y optimización de lógica aplicada

--- src/js/index.js
+++ src/js/index.js
@@ -12,51 +12,81 @@ let num4 = 0;
 let num5 = 0;
 let num6 = 0;
 
+// setInterval(() => {
+// 	if (num1 < 9) {
+// 		num1++;
+// 		ReactDOM.createRoot(document.getElementById("app")).render(<SecondsCounter n1={num1} n2={num2} n3={num3} n4={num4} n5={num5} n6={num6} />);
+// 	} else if (num1 == 9) {
+// 		num1 = 0;
+// 	}
+// }, 1000);
+// setInterval(() => {
+// 	if (num2 < 9) {
+// 		num2++;
+// 		ReactDOM.createRoot(document.getElementById("app")).render(<SecondsCounter n1={num1} n2={num2} n3={num3} n4={num4} n5={num5} n6={num6} />);
+// 	} else if (num2 == 9) {
+// 		num2 = 0;
+// 	}
+// }, 10000);
+// setInterval(() => {
+// 	if (num3 < 9) {
+// 		num3++;
+// 		ReactDOM.createRoot(document.getElementById("app")).render(<SecondsCounter n1={num1} n2={num2} n3={num3} n4={num4} n5={num5} n6={num6} />);
+// 	} else if (num3 == 9) {
+// 		num3 = 0;
+// 	}
+// }, 100000);
+// setInterval(() => {
+// 	if (num4 < 9) {
+// 		num4++;
+// 		ReactDOM.createRoot(document.getElementById("app")).render(<SecondsCounter n1={num1} n2={num2} n3={num3} n4={num4} n5={num5} n6={num6} />);
+// 	} else if (num4 == 9) {
+// 		num4 = 0;
+// 	}
+// }, 1000000);
+// setInterval(() => {
+// 	if (num5 < 9) {
+// 		num5++;
+// 		ReactDOM.createRoot(document.getElementById("app")).render(<SecondsCounter n1={num1} n2={num2} n3={num3} n4={num4} n5={num5} n6={num6} />);
+// 	} else if (num5 == 9) {
+// 		num5 = 0;
+// 	}
+// }, 10000000);
+// setInterval(() => {
+// 	if (num6 < 9) {
+// 		num6++;
+// 		ReactDOM.createRoot(document.getElementById("app")).render(<SecondsCounter n1={num1} n2={num2} n3={num3} n4={num4} n5={num5} n6={num6} />);
+// 	} else if (num6 == 9) {
+// 		num6 = 0;
+// 	}
+// }, 100000000);
+
 setInterval(() => {
-	if (num1 < 9) {
+	if (num1 < 10) {
 		num1++;
+		if (num1 > 9) {
+			num2++;
+			num1 = 0;
+			if (num2 > 9) {
+				num3++;
+				num2 = 0;
+				if (num3 > 9) {
+					num4++;
+					num3 = 0;
+					if (num4 > 9) {
+						num5++;
+						num4 = 0;
+						if (num5 > 9) {
+							num6++;
+							num5 = 0;
+							if (num6 > 9) {
+								num6 = 0;
+							}
+						}
+					}
+				}
+			}
+		}
 		ReactDOM.createRoot(document.getElementById("app")).render(<SecondsCounter n1={num1} n2={num2} n3={num3} n4={num4} n5={num5} n6={num6} />);
-	} else if (num1 == 9) {
-		num1 = 0;
 	}
 }, 1000);
-setInterval(() => {
-	if (num2 < 9) {
-		num2++;
-		ReactDOM.createRoot(document.getElementById("app")).render(<SecondsCounter n1={num1} n2={num2} n3={num3} n4={num4} n5={num5} n6={num6} />);
-	} else if (num2 == 9) {
-		num2 = 0;
-	}
-}, 10000);
-setInterval(() => {
-	if (num3 < 9) {
-		num3++;
-		ReactDOM.createRoot(document.getElementById("app")).render(<SecondsCounter n1={num1} n2={num2} n3={num3} n4={num4} n5={num5} n6={num6} />);
-	} else if (num3 == 9) {
-		num3 = 0;
-	}
-}, 100000);
-setInterval(() => {
-	if (num4 < 9) {
-		num4++;
-		ReactDOM.createRoot(document.getElementById("app")).render(<SecondsCounter n1={num1} n2={num2} n3={num3} n4={num4} n5={num5} n6={num6} />);
-	} else if (num4 == 9) {
-		num4 = 0;
-	}
-}, 1000000);
-setInterval(() => {
-	if (num5 < 9) {
-		num5++;
-		ReactDOM.createRoot(document.getElementById("app")).render(<SecondsCounter n1={num1} n2={num2} n3={num3} n4={num4} n5={num5} n6={num6} />);
-	} else if (num5 == 9) {
-		num5 = 0;
-	}
-}, 10000000);
-setInterval(() => {
-	if (num6 < 9) {
-		num6++;
-		ReactDOM.createRoot(document.getElementById("app")).render(<SecondsCounter n1={num1} n2={num2} n3={num3} n4={num4} n5={num5} n6={num6} />);
-	} else if (num6 == 9) {
-		num6 = 0;
-	}
-}, 100000000);
